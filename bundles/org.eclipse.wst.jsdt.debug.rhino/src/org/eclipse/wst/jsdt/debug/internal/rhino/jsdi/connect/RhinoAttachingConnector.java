ca1231ecace59e11f63034d67fbbf0beea7380ea
tommaso.vicarelli <tommaso.vicarelli@W09.fhoster.com>
2013-01-23 17:07:58 +0100

    added Secure and Trusted connection with SSL Socket added Fhoster Connector

--- bundles/org.eclipse.wst.jsdt.debug.rhino/src/org/eclipse/wst/jsdt/debug/internal/rhino/jsdi/connect/RhinoAttachingConnector.java
+++ bundles/org.eclipse.wst.jsdt.debug.rhino/src/org/eclipse/wst/jsdt/debug/internal/rhino/jsdi/connect/RhinoAttachingConnector.java
@@ -24,7 +24,8 @@ import org.eclipse.wst.jsdt.debug.transport.DebugSession;
 import org.eclipse.wst.jsdt.debug.transport.TransportService;
 
 /**
- * Rhino {@link Connector} implementation that attaches to a running debug process
+ * Rhino {@link Connector} implementation that attaches to a running debug
+ * process
  * 
  * @since 1.0
  * @see Connector
@@ -32,51 +33,65 @@ import org.eclipse.wst.jsdt.debug.transport.TransportService;
  */
 public class RhinoAttachingConnector implements AttachingConnector {
 
-	/**
-	 * Describes the <code>localhost</code> hostname i.e. the <code>127.0.0.1</code> IP address
-	 */
-	public static final String LOCALHOST = "localhost"; //$NON-NLS-1$
+    /**
+     * Describes the <code>localhost</code> hostname i.e. the
+     * <code>127.0.0.1</code> IP address
+     */
+    public static final String LOCALHOST = "localhost"; //$NON-NLS-1$
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.core.jsdi.connect.AttachingConnector#attach(java.util.Map)
-	 */
-	public VirtualMachine attach(Map arguments) throws IOException {
-		TransportService service = new RhinoTransportService();
-		String host = (String) arguments.get(HostArgument.HOST);
-		String port = (String) arguments.get(PortArgument.PORT);
-		Connection c = service.attach(host + ":" + Integer.parseInt(port), 10000, 10000); //$NON-NLS-1$
-		DebugSession session = new DebugSession(c);
-		return new VirtualMachineImpl(session);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.core.jsdi.connect.AttachingConnector#attach
+     * (java.util.Map)
+     */
+    public VirtualMachine attach(Map arguments) throws IOException {
+        TransportService service = new RhinoTransportService();
+        String host = (String) arguments.get(HostArgument.HOST);
+        String port = (String) arguments.get(PortArgument.PORT);
+        Connection c = service.attach(host + ":" + Integer.parseInt(port), null, null, 10000, 10000); //$NON-NLS-1$
+        DebugSession session = new DebugSession(c);
+        return new VirtualMachineImpl(session);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.core.jsdi.connect.Connector#defaultArguments()
-	 */
-	public Map defaultArguments() {
-		Map result = new HashMap();
-		result.put(HostArgument.HOST, new HostArgument(LOCALHOST));
-		result.put(PortArgument.PORT, new PortArgument(9000));
-		return result;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.core.jsdi.connect.Connector#defaultArguments()
+     */
+    public Map defaultArguments() {
+        Map result = new HashMap();
+        result.put(HostArgument.HOST, new HostArgument(LOCALHOST));
+        result.put(PortArgument.PORT, new PortArgument(9000));
+        return result;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.core.jsdi.connect.Connector#description()
-	 */
-	public String description() {
-		return Messages.RhinoAttachingConnector_description;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see fhoster.eclipse.debug.core.jsdi.connect.Connector#description()
+     */
+    public String description() {
+        return Messages.RhinoAttachingConnector_description;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.core.jsdi.connect.Connector#name()
-	 */
-	public String name() {
-		return Messages.RhinoAttachingConnector_name;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see fhoster.eclipse.debug.core.jsdi.connect.Connector#name()
+     */
+    public String name() {
+        return Messages.RhinoAttachingConnector_name;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.core.jsdi.connect.Connector#id()
-	 */
-	public String id() {
-		return "rhino.attaching.connector"; //$NON-NLS-1$
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see fhoster.eclipse.debug.core.jsdi.connect.Connector#id()
+     */
+    public String id() {
+        return "rhino.attaching.connector"; //$NON-NLS-1$
+    }
 }
--- bundles/org.eclipse.wst.jsdt.debug.transport/src/org/eclipse/wst/jsdt/debug/transport/socket/SocketTransportService.java
+++ bundles/org.eclipse.wst.jsdt.debug.transport/src/org/eclipse/wst/jsdt/debug/transport/socket/SocketTransportService.java
@@ -20,135 +20,180 @@ import org.eclipse.wst.jsdt.debug.transport.ListenerKey;
 import org.eclipse.wst.jsdt.debug.transport.TransportService;
 
 /**
- * Implementation of a {@link TransportService} that using a {@link Socket} for communication
+ * Implementation of a {@link TransportService} that using a {@link Socket} for
+ * communication
  * 
  * @since 1.0
  */
 public abstract class SocketTransportService implements TransportService {
 
-	/**
-	 * Map of {@link ListenerKey} to {@link ServerSocket}s
-	 */
-	private Map listeners = new HashMap();
+    /**
+     * Map of {@link ListenerKey} to {@link ServerSocket}s
+     */
+    private final Map listeners = new HashMap();
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.transport.TransportService#startListening(java.lang.String)
-	 */
-	public synchronized ListenerKey startListening(String address) throws IOException {
-		String host = null;
-		int port = 0;
-		if (address != null) {
-			String[] strings = address.split(Constants.COLON);
-			if (strings.length == 2) {
-				host = strings[0];
-				port = Integer.parseInt(strings[1]);
-			} else {
-				port = Integer.parseInt(strings[0]);
-			}
-		}
-		if (host == null) {
-			host = Constants.LOCALHOST;
-		}
-		ListenerKey key = new SocketListenerKey(host + Constants.COLON + port);
-		ServerSocket serverSocket = new ServerSocket(port);
-		listeners.put(key, serverSocket);
-		return key;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.transport.TransportService#startListening(
+     * java.lang.String)
+     */
+    public synchronized ListenerKey startListening(String address) throws Exception {
+        String host = null;
+        int port = 0;
+        if (address != null) {
+            String[] strings = address.split(Constants.COLON);
+            if (strings.length == 2) {
+                host = strings[0];
+                port = Integer.parseInt(strings[1]);
+            }
+            else {
+                port = Integer.parseInt(strings[0]);
+            }
+        }
+        if (host == null) {
+            host = Constants.LOCALHOST;
+        }
+        ListenerKey key = new SocketListenerKey(host + Constants.COLON + port);
+        ServerSocket serverSocket = this.createServerSocket(port);
+        this.listeners.put(key, serverSocket);
+        return key;
 
-	};
+    }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.transport.TransportService#stopListening(org.eclipse.wst.jsdt.debug.transport.TransportService.ListenerKey)
-	 */
-	public void stopListening(ListenerKey key) throws IOException {
-		ServerSocket serverSocket = (ServerSocket) listeners.remove(key);
-		if (serverSocket != null) {
-			serverSocket.close();
-		}
-	}
+    protected abstract ServerSocket createServerSocket(int port) throws Exception;
 
-	/**
-	 * Returns the {@link ServerSocket} mapped to the given {@link ListenerKey} or 
-	 * <code>null</code> if the mapping does not exist
-	 * 
-	 * @param key the {@link ListenerKey} to get the {@link ServerSocket} for
-	 * @return the mapped {@link ServerSocket} or <code>null</code>
-	 */
-	public final ServerSocket getServerSocket(ListenerKey key) {
-		synchronized (listeners) {
-			return (ServerSocket) listeners.get(key);
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.transport.TransportService#accept(org.eclipse.wst.jsdt.debug.transport.TransportService.ListenerKey, long, long)
-	 */
-	public Connection accept(ListenerKey key, long attachTimeout, long handshakeTimeout) throws IOException {
-		ServerSocket serverSocket = getServerSocket(key);
-		if (serverSocket == null) {
-			throw new IllegalStateException("Accept failed. Not listening for address: key.address()"); //$NON-NLS-1$
-		}
-		int timeout = (int) attachTimeout;
-		if (timeout > 0) {
-			if (timeout > Integer.MAX_VALUE) {
-				timeout = Integer.MAX_VALUE; // approximately 25 days!
-			}
-			serverSocket.setSoTimeout(timeout);
-		}
-		Connection connection = getConnection(serverSocket.accept());
-		handleAccept(connection);
-		return connection;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.transport.TransportService#stopListening(org
+     * .eclipse.wst.jsdt.debug.transport.TransportService.ListenerKey)
+     */
+    public void stopListening(ListenerKey key) throws IOException {
+        ServerSocket serverSocket = (ServerSocket) this.listeners.remove(key);
+        if (serverSocket != null) {
+            serverSocket.close();
+        }
+    }
 
-	/**
-	 * This method allows implementors to perform specific actions immediately following
-	 * a successful accept for {@link Socket} communication (the socket has been successfully opened).
-	 * 
-	 * @param connection the connection that just opened
-	 * @throws IOException
-	 */
-	public abstract void handleAccept(Connection connection) throws IOException;
-	
-	/**
-	 * Returns the {@link SocketConnection} for the given {@link Socket}.
-	 * <br><br>
-	 * This method cannot return <code>null</code>, if a connection cannot be created
-	 * an {@link IOException} must be thrown.
-	 * 
-	 * @param socket the socket to get the connection for
-	 * @return the {@link SocketConnection} for the given {@link Socket}, never <code>null</code>
-	 * @throws IOException
-	 */
-	public abstract SocketConnection getConnection(Socket socket) throws IOException;
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.jsdt.debug.transport.TransportService#attach(java.lang.String, long, long)
-	 */
-	public Connection attach(String address, long attachTimeout, long handshakeTimeout) throws IOException {
-		String host = null;
-		int port = 0;
-		if (address != null) {
-			String[] strings = address.split(Constants.COLON);
-			if (strings.length == 2) {
-				host = strings[0];
-				port = Integer.parseInt(strings[1]);
-			} else {
-				port = Integer.parseInt(strings[0]);
-			}
-		}
-		if (host == null) {
-			host = Constants.LOCALHOST;
-		}
-		Connection connection = getConnection(new Socket(host, port));
-		handleAttach(connection);
-		return connection;
-	}
-	
-	/**
-	 * This method allows implementors to perform specific actions immediately following
-	 * a successful attach for {@link Socket} communication (the socket has been successfully opened).
-	 * 
-	 * @param connection the connection that just opened
-	 * @throws IOException
-	 */
-	public abstract void handleAttach(Connection connection) throws IOException;
+    /**
+     * Returns the {@link ServerSocket} mapped to the given {@link ListenerKey}
+     * or <code>null</code> if the mapping does not exist
+     * 
+     * @param key
+     *            the {@link ListenerKey} to get the {@link ServerSocket} for
+     * @return the mapped {@link ServerSocket} or <code>null</code>
+     */
+    public final ServerSocket getServerSocket(ListenerKey key) {
+        synchronized (this.listeners) {
+            return (ServerSocket) this.listeners.get(key);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.transport.TransportService#accept(org.eclipse
+     * .wst.jsdt.debug.transport.TransportService.ListenerKey, long, long)
+     */
+    public Connection accept(ListenerKey key, long attachTimeout, long handshakeTimeout) throws IOException {
+        ServerSocket serverSocket = this.getServerSocket(key);
+        if (serverSocket == null) {
+            throw new IllegalStateException("Accept failed. Not listening for address: key.address()"); //$NON-NLS-1$
+        }
+        int timeout = (int) attachTimeout;
+        if (timeout > 0) {
+            if (timeout > Integer.MAX_VALUE) {
+                timeout = Integer.MAX_VALUE; // approximately 25 days!
+            }
+            serverSocket.setSoTimeout(timeout);
+        }
+        Connection connection = this.getConnection(serverSocket.accept());
+        this.handleAccept(connection);
+        return connection;
+    }
+
+    /**
+     * This method allows implementors to perform specific actions immediately
+     * following
+     * a successful accept for {@link Socket} communication (the socket has been
+     * successfully opened).
+     * 
+     * @param connection
+     *            the connection that just opened
+     * @param password
+     * @param username
+     * @throws IOException
+     */
+    public abstract void handleAccept(Connection connection) throws IOException;
+
+    /**
+     * Returns the {@link SocketConnection} for the given {@link Socket}. <br>
+     * <br>
+     * This method cannot return <code>null</code>, if a connection cannot be
+     * created
+     * an {@link IOException} must be thrown.
+     * 
+     * @param socket
+     *            the socket to get the connection for
+     * @return the {@link SocketConnection} for the given {@link Socket}, never
+     *         <code>null</code>
+     * @throws IOException
+     */
+    public abstract SocketConnection getConnection(Socket socket) throws IOException;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * fhoster.eclipse.debug.transport.TransportService#attach(java.lang
+     * .String, long, long)
+     */
+
+    public Connection attach(String address, String username, String password, long attachTimeout, long handshakeTimeout) throws IOException {
+        String host = null;
+        int port = 0;
+        if (address != null) {
+            String[] strings = address.split(Constants.COLON);
+            if (strings.length == 2) {
+                host = strings[0];
+                port = Integer.parseInt(strings[1]);
+            }
+            else {
+                port = Integer.parseInt(strings[0]);
+            }
+        }
+        if (host == null) {
+            host = Constants.LOCALHOST;
+        }
+        Connection connection = null;
+        try {
+            connection = this.getConnection(this.createSocket(host, port));
+            this.handleAttach(connection, username, password);
+        }
+        catch (Exception e) {
+            //TODO: manage exception
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    protected abstract Socket createSocket(String host, int port) throws Exception;
+
+    /**
+     * This method allows implementors to perform specific actions immediately
+     * following
+     * a successful attach for {@link Socket} communication (the socket has been
+     * successfully opened).
+     * 
+     * @param connection
+     *            the connection that just opened
+     * @param password
+     * @param username
+     * @throws Exception
+     */
+    public abstract void handleAttach(Connection connection, String username, String password) throws Exception;
 }
